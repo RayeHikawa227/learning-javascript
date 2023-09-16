25be689bb728b53caaad44b4dd48bac98e39806e
Arn Christian <80439628+RayeHikawa227@users.noreply.github.com>
2023-09-16 12:24:05 +0800

    Add files via upload

---
+++ java.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class java {
+    public static void main(String [] args) {
+        System.out.println("Fuck you");
+        Scanner $new = new Scanner(System.in);
+        String name = $new.nextLine();
+        System.out.println(name);
+    }
+}
