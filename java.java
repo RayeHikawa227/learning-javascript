d9fa8ddaf693197fffd18530cb5142e22f1d704a
Arn Christian <80439628+RayeHikawa227@users.noreply.github.com>
2023-09-17 22:35:08 +0800

    Delete java.java

--- java.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class java {
-    public static void main(String [] args) {
-        System.out.println("Fuck you");
-        Scanner $new = new Scanner(System.in);
-        String name = $new.nextLine();
-        System.out.println(name);
-    }
-}
